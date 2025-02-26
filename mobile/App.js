94c70a838b880dead64d33f0f99a53b8ee90875c
CaioDalNegro <caiodalnegro@gmail.com>
2025-02-25 22:19:46 -0300

    Subindo visual do app

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { View, Text, TextInput, FlatList, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { 
+  View, Text, TextInput, FlatList, Image, TouchableOpacity, StyleSheet 
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 // Dados de exemplo de locais de hospedagem e tours virtuais
 const places = [
-  { id: "1", name: "Nome da pousada", location: "Jakarta, Indonesia", image: "https://via.placeholder.com/150" },
-  { id: "2", name: "Nome da pousada", location: "Jakarta, Indonesia", image: "https://via.placeholder.com/150" },
-];
-
-const virtualTours = [
-  { id: "1", name: "Emerald Hotel", location: "Jakarta, Indonesia", image: "https://via.placeholder.com/150" },
-  { id: "2", name: "Fransissco Hotel", location: "Jakarta, Indonesia", image: "https://via.placeholder.com/150" },
+  { id: "1", name: "Domo", location: "Laguna-SC", image: "./assets/image/domo_1.jpeg" },
+  { id: "2", name: "Charrua (Bus)", location: "Laguna-SC", image: "./assets/image/bus_1.jpeg" },
 ];
 
 // Componente principal da tela inicial
@@ -20,14 +17,24 @@ export default function HomeScreen() {
       {/* Cabeçalho */}
       <View style={styles.header}>
         <View style={styles.headerTop}>
-          {/* Texto e ícone de notificação */}
-          <Text style={styles.headerText}>Seu refúgio perfeito a <Text style={styles.boldText}>um clique</Text> de distância!</Text>
+          <Text style={styles.headerText}>
+            Seu refúgio perfeito a <Text style={styles.boldText}>um clique</Text> de distância!
+          </Text>
           <Ionicons name="notifications-outline" size={24} color="white" />
         </View>
+        
         {/* Caixa de pesquisa */}
-        <View style={styles.searchBox}>
-          <TextInput placeholder="Pesquisar" style={styles.searchInput} />
-          <Ionicons name="search" size={20} color="gray" />
+        <View style={styles.searchContainer}>
+          <View style={styles.searchBox}>
+            <TextInput 
+              placeholder="Pesquisar" 
+              placeholderTextColor="white" 
+              style={styles.searchInput} 
+            />
+          </View>
+          <TouchableOpacity style={styles.searchButton}>
+            <Ionicons name="search" size={24} color="white" />
+          </TouchableOpacity>
         </View>
       </View>
       
@@ -35,10 +42,9 @@ export default function HomeScreen() {
       <Text style={styles.sectionTitle}>Nossas hospedagens</Text>
       <FlatList
         horizontal
-        data={places} // A lista de locais de hospedagem é passada como dados
-        keyExtractor={(item) => item.id} // A chave única para cada item
+        data={places}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          // Renderiza um cartão para cada lugar
           <View style={styles.card}>
             <Image source={{ uri: item.image }} style={styles.image} />
             <Text style={styles.cardTitle}>{item.name}</Text>
@@ -52,16 +58,83 @@ export default function HomeScreen() {
 
 // Estilos para os componentes
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#f3f4f6", padding: 16 }, // Estilo da tela principal
-  header: { backgroundColor: "#4caf50", padding: 16, borderRadius: 16}, // Estilo do cabeçalho
-  headerTop: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" }, // Alinha texto e ícone
-  headerText: { color: "white", fontSize: 20, fontWeight: "regular", fontFamily: "Raleway" }, // Estilo do texto do cabeçalho
-  boldText: { fontWeight: "900" }, // Estilo do texto em negrito
-  searchBox: { flexDirection: "row", alignItems: "center", backgroundColor: "white", padding: 10, borderRadius: 12, marginTop: 10 }, // Caixa de pesquisa
-  searchInput: { flex: 1, color: "#333" }, // Estilo do campo de texto
-  sectionTitle: { fontSize: 18, fontWeight: "bold", marginTop: 20 }, // Título da seção
-  card: { margin: 8 }, // Estilo do cartão de hospedagem
-  image: { width: 128, height: 80, borderRadius: 8 }, // Estilo da imagem no cartão
-  cardTitle: { fontSize: 14, fontWeight: "bold" }, // Título do cartão
-  cardSubtitle: { fontSize: 12, color: "#666" }, // Subtítulo do cartão
+  container: {
+    flex: 1, 
+    backgroundColor: "#f3f4f6",
+  },
+  header: {
+    backgroundColor: "#4CA69A", 
+    padding: 40, 
+    borderBottomRightRadius: 45
+  },
+  headerTop: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    alignItems: "center"
+  },
+  headerText: {
+    color: "white", 
+    fontSize: 20, 
+    fontWeight: "regular", 
+    fontFamily: "Raleway",  
+    marginTop: 80, 
+    marginBottom: 20
+  },
+  boldText: {
+    fontWeight: "900"
+  },
+  
+  // Estilização da caixa de pesquisa
+  searchContainer: { 
+    flexDirection: "row", 
+    alignItems: "center",
+    marginTop: 10,
+  },
+  searchBox: { 
+    flex: 1,
+    borderWidth: 1, 
+    borderColor: "white", 
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  searchInput: { 
+    color: "white"
+  },
+  searchButton: { 
+    width: 48, 
+    height: 48, 
+    backgroundColor: "#2194FF", 
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    marginLeft: 8,
+  },
+  
+  // Seção de hospedagens
+  sectionTitle: {
+    fontSize: 18, 
+    fontWeight: "bold", 
+    //marginTop: 20,
+    marginLeft:40,
+    padding: 10,
+  },
+  card: {
+    margin: 8,
+    marginLeft:40,
+  },
+  image: {
+    width: 128, 
+    height: 80, 
+    borderRadius: 8,
+    backgroundColor: "gray"
+  },
+  cardTitle: {
+    fontSize: 14, 
+    fontWeight: "bold"
+  },
+  cardSubtitle: {
+    fontSize: 12, 
+    color: "#666"
+  }
 });
