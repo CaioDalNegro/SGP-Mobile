439c21b36b7564bce4f53143d607f440da83b276
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-22 21:51:53 -0300

    atualizado

--- mobile/navigation/MainTabs.js
+++ mobile/navigation/MainTabs.js
@@ -11,16 +11,10 @@ const Tab = createBottomTabNavigator();
 export default function MainTabs() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-<<<<<<< HEAD
-      <Tab.Screen name="Home" component={HomeScreen} />
-      {<Tab.Screen name="FavoriteScreen" component={FavoriteScreen}/>}
-      {<Tab.Screen name="PerfilScreen" component={PerfilScreen}/>}
-      {<Tab.Screen name="Reservas" component={ReservaScreen}/>}
-=======
       <Tab.Screen name="Início" component={HomeScreen} />
+      {<Tab.Screen name="FavoriteScreen" component={FavoriteScreen}/>}
       {<Tab.Screen name="Favoritos" component={FavoriteScreen}/>}
-      {<Tab.Screen name="Perfil" component={PerfilScreen}/>}
->>>>>>> 1965aee964859f935cf92ebef522c67b7b1a5ed3
+      {<Tab.Screen name="Reservas" component={ReservaScreen}/>}
     </Tab.Navigator>
   );
 }
